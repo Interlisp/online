1e9661582bc797ee3b70c41f8151cc78b2e9f840
Frank Halasz <frank@halasz.org>
2022-10-12 02:00:41 +0000

    WIP progress commit.  Initial changes for oio HELPSYS support.

---
+++ docker_medley/scripts/request_new_tab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#    Request (modified) noVNC to open a new tab in its containing browser and load the URL
+#    given as the only argment to this script.
+#
+#    Uses vncconfig and the desktop name setting as the out-of-band channel to get the message from
+#    the online server to the client browser running the noVNC modified to process these ot-of-band
+#    messages.
+#
+#    Intended for use by the HELPSYS LispUsers package to open Common Lisp Hyperspec documentation for
+#    online.interlisp.org. 
+#
+#    2022-10-11 Frank Halasz
+#
+#
+URL=5d4f26d9d86696b6$1
+/usr/bin/vncconfig -display ${DISPLAY} -set desktop="${URL}"
+
