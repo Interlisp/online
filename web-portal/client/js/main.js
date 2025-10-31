3d4939177ebc1a69003531ddafb606d16ef24d82
Frank Halasz <frank@halasz.org>
2025-10-31 22:07:27 +0000

    Adding some debugging code

--- web-portal/client/js/main.js
+++ web-portal/client/js/main.js
@@ -171,7 +171,7 @@ function startSession (interlispOrXterm) {
                             + `&sftp=${startSftp || "false"}`
                             + `&exec=${medleyExec || "inter"}`
                             + ( isAutoLogin ? "&autologin" : "")
-                            + ( (alStart != "") ? `&start=${alStart}` : "" )
+                            + ( (alStart != "") ? `&start=${encodeURIComponent(alStart)}` : "" )
 			;
                      console.log(medley_url);
 if(false){
